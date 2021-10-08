63dc97e52897a4bf7d1c25a4db7205291314ac95
anurag-saraswat <saraswatanurag14@gmail.com>
2021-10-08 13:54:54 +0530

    Quiz2

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,6 +2,7 @@ import sys
 import os
 import warnings
 from sklearn.utils.random import sample_without_replacement
+import math
 
 warnings.filterwarnings("ignore")
 
@@ -9,17 +10,73 @@ base_path = '/home/anurag/Desktop/ML_ops/MNIST_Example/mnist'
 sys.path.append(base_path)
 from plot_graph import classification_task
 from sklearn import datasets
+from utils import create_splits
 
 
-def test_model_writing():
+
+## Made up generic test case for bonus
+
+def test_create_split_bonus():
 	digits = datasets.load_digits()
-	metric , model_path = classification_task(digits)
-	final_file = '../mnist/' + model_path[1:] 
-	assert os.path.isdir(final_file)
+	n_samples = len(digits.images)
+	val_ratio = 0.2
+	test_ratio = 0.1
+	train_ratio = 1 - val_ratio - test_ratio
 
-def test_small_data_overfit_checking():
+	train_sample = math.ceil(n_samples *train_ratio)
+	test_sample = math.ceil(n_samples *test_ratio)
+	val_sample = math.ceil(n_samples *val_ratio)
+
+	actual_train ,actual_test , actual_valid ,_ ,_ ,_= create_splits(digits.images, digits.target, test_ratio, val_ratio)    
+    
+	total = len(actual_train) + len(actual_test) + len(actual_valid) 
+
+	assert train_sample == len(actual_train)
+	assert test_sample == len(actual_test)
+	assert val_sample == len(actual_valid)
+	assert n_samples == total
+
+
+def test_create_split_1():
 	digits = datasets.load_digits()
-	metrics , _ = classification_task(digits,isTrain=True)
-	assert metrics['acc'] > 0.9
-	assert metrics['f1'] > 0.9
+	n_samples = 100
+	val_ratio = 0.7
+	test_ratio = 0.2
+	train_ratio = 1 - val_ratio - test_ratio
+
+	train_sample = int(n_samples *train_ratio)
+	test_sample = int(n_samples *test_ratio)
+	val_sample = int(n_samples *val_ratio)
+
+	actual_train ,actual_test , actual_valid ,_ ,_ ,_= create_splits(digits.images[:n_samples ], digits.target[:n_samples ], test_ratio, val_ratio)    
+    
+	total = len(actual_train) + len(actual_test) + len(actual_valid) 
+
+	assert train_sample == len(actual_train)
+	assert test_sample == len(actual_test)
+	assert val_sample == len(actual_valid)
+	assert n_samples == total
+
+def test_create_split_2():
+	digits = datasets.load_digits()
+	n_samples = 9
+	val_ratio = 0.7
+	test_ratio = 0.2
+	train_ratio = 1 - val_ratio - test_ratio
+
+	train_sample = math.ceil(n_samples *train_ratio)
+	test_sample = math.ceil(n_samples *test_ratio)
+	val_sample = math.ceil(n_samples *val_ratio)
+
+	actual_train ,actual_test , actual_valid = create_splits(digits.images[:n_samples ], digits.target[:n_samples ], test_ratio, val_ratio,case = True)    
+    
+	total = actual_train +actual_test + actual_valid 
+
+	#assert train_sample == len(actual_train)
+	#assert test_sample == len(actual_test)
+	#assert val_sample == len(actual_valid)
+	#assert n_samples == total
+
+#test_create_split_2()
+
 
